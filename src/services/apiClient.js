b6644daa9f918c6e324eaa939e5dc9dd5f1f04f0
rohan288230 <rohansinghramola9312@gmail.com>
2026-05-30 14:12:42 +0530

    fixed link

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { ROUTES } from '../constants/routes.js';
 
 const apiClient = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
